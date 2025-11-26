4be405d7ed412c2256354c24c9a6853b10b8e4fd
Leon Ahuja <leon.ahuja@tandm.ai>
2025-11-26 15:43:26 +1000

    poosh rect sample

--- ddrOCR.m
+++ ddrOCR.m
@@ -120,3 +120,14 @@ auto_stats = regionprops(BW);
 % So basically regionprops with no CC provided manually just uses bwconncomp under the hood
 manual_stats = regionprops(CC);
 
+
+%% Get HRECT(tx, ty, w, h) whcih is different from roi (corner verteces)
+figure;
+imshow(img);
+h_rect = imrect();
+% Rectangle position is given as [xmin, ymin, width, height]
+pos_rect = h_rect.getPosition();
+% Round off so the coordinates can be used as indices
+pos_rect = round(pos_rect);
+% Select part of the image
+img_cropped = img(pos_rect(2) + (0:pos_rect(4)), pos_rect(1) + (0:pos_rect(3)));
